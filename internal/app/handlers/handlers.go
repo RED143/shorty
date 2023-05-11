b1bc9ed02948b337902b76ce78cf5bb16225c17e
RED143 <b.zabalotny@gmail.com>
2023-05-11 22:42:50 +0300

    fix: fix errors return

--- internal/app/handlers/handlers.go
+++ internal/app/handlers/handlers.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"fmt"
 	"github.com/go-chi/chi/v5"
 	"io"
 	"net/http"
@@ -14,44 +13,40 @@ import (
 func Shortify(writer http.ResponseWriter, request *http.Request) {
 	if request.Method != http.MethodPost {
 		http.Error(writer, "Only POST requests are allowed", http.StatusBadRequest)
-		return
 	}
 
 	body, err := io.ReadAll(request.Body)
 	if err != nil {
-		fmt.Errorf("failed to parse body: %w", err)
+		http.Error(writer, "Failed to parse request body", http.StatusInternalServerError)
 	}
 	hashString := hash.Generate(body)
 	storage.SetValue(hashString, string(body))
 
 	if len(body) == 0 {
 		http.Error(writer, "URL should be provided", http.StatusBadRequest)
-		return
 	}
 
 	cfg := config.GetConfig()
-	fullUrl, err := url.JoinPath(cfg.BaseAddress, hashString)
+	fullURL, err := url.JoinPath(cfg.BaseAddress, hashString)
 	if err != nil {
-		fmt.Errorf("failed to joinPath(%s, %s): %w", cfg.BaseAddress, hashString, err)
+		http.Error(writer, "Failed to parse request body", http.StatusInternalServerError)
 	}
 
 	writer.Header().Set("content-type", "plain/text")
 	writer.WriteHeader(http.StatusCreated)
-	writer.Write([]byte(fullUrl))
+	writer.Write([]byte(fullURL))
 }
 
 func GetLink(writer http.ResponseWriter, request *http.Request) {
 	if request.Method != http.MethodGet {
 		http.Error(writer, "Only GET requests are allowed", http.StatusBadRequest)
-		return
 	}
 
 	hash := chi.URLParam(request, "hash")
-	link := storage.GetValue(hash)
+	link, ok := storage.GetValue(hash)
 
-	if link == "" {
+	if ok == false {
 		http.Error(writer, "Link not found", http.StatusBadRequest)
-		return
 	}
 
 	writer.Header().Set("location", link)
--- internal/app/storage/storage.go
+++ internal/app/storage/storage.go
@@ -1,11 +1,17 @@
 package storage
 
-var storage = make(map[string]string)
+import (
+	"fmt"
+	"sync"
+)
+
+var storage = sync.Map{}
 
 func SetValue(key, value string) {
-	storage[key] = value
+	storage.Store(key, value)
 }
 
-func GetValue(key string) string {
-	return storage[key]
+func GetValue(key string) (string, bool) {
+	value, ok := storage.Load(key)
+	return fmt.Sprint(value), ok
 }
