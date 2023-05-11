6a7f4b0ff8086bd21d9c781fdd835dfb3f396401
RED143 <b.zabalotny@gmail.com>
2023-05-11 15:00:37 +0300

    fix: update config package

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -1,77 +1,33 @@
 package config
 
 import (
-	"errors"
 	"flag"
-	"net"
 	"os"
-	"strconv"
-	"strings"
 )
 
-var Port = 8080
-var Host = "localhost"
-var BaseAddress = "localhost:8080"
-var Scheme = "http://"
-
-func GetServerAddress() string {
-	return Host + ":" + strconv.Itoa(Port)
+type config struct {
+	ServerAddress string
+	BaseAddress   string
 }
 
-func InitConfig() {
-	getServerAddress()
-	getBaseAddress()
+var cfg = config{}
+
+func ParseFlags() {
+	flag.StringVar(&cfg.ServerAddress, "a", "localhost:8080", "set a server address")
+	flag.StringVar(&cfg.BaseAddress, "b", "http://localhost:8080", "set a base address")
 	flag.Parse()
 }
 
-func getServerAddress() error {
-
-	flag.Func("a", "set a server address", func(serverAddress string) error {
-		defaultAddress := serverAddress
-		envAddress := os.Getenv("SERVER_ADDRESS")
-
-		if envAddress != "" {
-			defaultAddress = envAddress
-		}
-
-		host, port, err := parseHostAndPort(defaultAddress)
-		if err != nil {
-			return err
-		}
-
-		Host = host
-		Port = port
-
-		return nil
-	})
-
-	return nil
-}
-
-func getBaseAddress() error {
-	defaultAddress := "localhost:8080"
-	envBaseURL := os.Getenv("BASE_URL")
-
-	if envBaseURL != "" {
-		defaultAddress = envBaseURL
+func GetConfig() config {
+	envServerAddress := os.Getenv("SERVER_ADDRESS")
+	if envServerAddress != "" {
+		cfg.ServerAddress = envServerAddress
 	}
 
-	flag.StringVar(&BaseAddress, "b", defaultAddress, "set a base address")
-
-	return nil
-}
-
-func parseHostAndPort(s string) (string, int, error) {
-	s = strings.Replace(s, Scheme, "", 1)
-	host, port, err := net.SplitHostPort(s)
-	if err != nil {
-		return "", 0, errors.New("invalid parsing url")
+	envBaseAddress := os.Getenv("BASE_URL")
+	if envBaseAddress != "" {
+		cfg.BaseAddress = envBaseAddress
 	}
 
-	parsedPort, err := strconv.Atoi(strings.ReplaceAll(port, "/", ""))
-	if err != nil {
-		return "", 0, errors.New("invalid port")
-	}
-
-	return host, parsedPort, err
+	return cfg
 }
--- internal/app/handlers/handlers.go
+++ internal/app/handlers/handlers.go
@@ -1,23 +1,26 @@
 package handlers
 
 import (
+	"fmt"
+	"github.com/go-chi/chi/v5"
 	"io"
 	"net/http"
-	"strings"
-
-	"github.com/RED143/shorty/internal/app/config"
-	"github.com/RED143/shorty/internal/app/hash"
-	"github.com/RED143/shorty/internal/app/storage"
-	"github.com/go-chi/chi/v5"
+	"net/url"
+	"shorty/internal/app/config"
+	"shorty/internal/app/hash"
+	"shorty/internal/app/storage"
 )
 
-func ShortifyHandler(writer http.ResponseWriter, request *http.Request) {
+func Shortify(writer http.ResponseWriter, request *http.Request) {
 	if request.Method != http.MethodPost {
 		http.Error(writer, "Only POST requests are allowed", http.StatusBadRequest)
 		return
 	}
 
-	body, _ := io.ReadAll(request.Body)
+	body, err := io.ReadAll(request.Body)
+	if err != nil {
+		fmt.Errorf("failed to parse body: %w", err)
+	}
 	hashString := hash.Generate(body)
 	storage.SetValue(hashString, string(body))
 
@@ -26,18 +29,18 @@ func ShortifyHandler(writer http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	writer.Header().Set("content-type", "plain/text")
-	writer.WriteHeader(http.StatusCreated)
-
-	response := config.BaseAddress + "/" + hashString
-	if !strings.Contains(config.BaseAddress, config.Scheme) {
-		response = config.Scheme + response
+	cfg := config.GetConfig()
+	fullUrl, err := url.JoinPath(cfg.BaseAddress, hashString)
+	if err != nil {
+		fmt.Errorf("failed to joinPath(%s, %s): %w", cfg.BaseAddress, hashString, err)
 	}
 
-	writer.Write([]byte(response))
+	writer.Header().Set("content-type", "plain/text")
+	writer.WriteHeader(http.StatusCreated)
+	writer.Write([]byte(fullUrl))
 }
 
-func LinkHandler(writer http.ResponseWriter, request *http.Request) {
+func GetLink(writer http.ResponseWriter, request *http.Request) {
 	if request.Method != http.MethodGet {
 		http.Error(writer, "Only GET requests are allowed", http.StatusBadRequest)
 		return
