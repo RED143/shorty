d358b4076da1684b1326662e5026bd43b335c51a
RED143 <b.zabalotny@gmail.com>
2023-05-17 09:08:09 +0300

    refactor: update handlers

--- internal/app/handlers/handlers.go
+++ internal/app/handlers/handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"io"
+	"log"
 	"net/http"
 	"net/url"
 	"shorty/internal/app/config"
@@ -9,44 +10,54 @@ import (
 	"shorty/internal/app/storage"
 )
 
-func Shortify(writer http.ResponseWriter, request *http.Request, cfg config.Config) {
+func Shortify(writer http.ResponseWriter, request *http.Request, cfg config.Config, str storage.Storage) {
 	if request.Method != http.MethodPost {
 		http.Error(writer, "Only POST requests are allowed", http.StatusBadRequest)
+		return
 	}
 
 	body, err := io.ReadAll(request.Body)
 	if err != nil {
-		http.Error(writer, "Failed to parse request body", http.StatusInternalServerError)
+		http.Error(writer, "Internal server error", http.StatusInternalServerError)
+		log.Println("failed to parse request body")
+		return
 	}
 	hashString := hash.Generate(body)
-	storage.Storage.Put(hashString, string(body))
+	str.Put(hashString, string(body))
 
 	if len(body) == 0 {
 		http.Error(writer, "URL should be provided", http.StatusBadRequest)
+		return
 	}
 
 	fullURL, err := url.JoinPath(cfg.BaseAddress, hashString)
 	if err != nil {
-		http.Error(writer, "Failed to generate path", http.StatusInternalServerError)
+		http.Error(writer, "Internal server error", http.StatusInternalServerError)
+		log.Println("failed to generate path")
+		return
 	}
 
 	writer.Header().Set("content-type", "plain/text")
 	writer.WriteHeader(http.StatusCreated)
 	_, err = writer.Write([]byte(fullURL))
 	if err != nil {
-		http.Error(writer, "Failed to write response", http.StatusInternalServerError)
+		http.Error(writer, "Internal server error", http.StatusInternalServerError)
+		log.Println("failed to write response")
+		return
 	}
 }
 
-func GetLink(writer http.ResponseWriter, request *http.Request, hash string) {
+func GetLink(writer http.ResponseWriter, request *http.Request, hash string, str storage.Storage) {
 	if request.Method != http.MethodGet {
 		http.Error(writer, "Only GET requests are allowed", http.StatusBadRequest)
+		return
 	}
 
-	link, ok := storage.Storage.Get(hash)
+	link, ok := str.Get(hash)
 
 	if !ok {
 		http.Error(writer, "Link not found", http.StatusBadRequest)
+		return
 	}
 
 	writer.Header().Set("location", link)
