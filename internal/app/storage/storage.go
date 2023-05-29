3d194cc15ac568e8a530f5482c063ad053bdce58
RED143 <b.zabalotny@gmail.com>
2023-05-29 21:57:00 +0300

    feat: change storage to file

--- internal/app/storage/storage.go
+++ internal/app/storage/storage.go
@@ -1,30 +1,91 @@
 package storage
 
 import (
+	"bufio"
+	"encoding/json"
+	"log"
+	"os"
+	"strconv"
 	"sync"
 )
 
 type Storage struct {
-	mu    *sync.Mutex
-	links map[string]string
+	mu       *sync.Mutex
+	filePath string
+}
+
+type Line struct {
+	UUID        string `json:"uuid"`
+	ShortUrl    string `json:"short_url"`
+	OriginalUrl string `json:"original_url"`
 }
 
 func (s *Storage) Put(key, value string) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
-	s.links[key] = value
+	file, err := os.OpenFile(s.filePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("failed to open storage file: %v", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	countLines := 0
+	isAlreadySaved := false
+
+	for scanner.Scan() {
+		line := Line{}
+		if err := json.Unmarshal(scanner.Bytes(), &line); err != nil {
+			log.Fatalf("failed to read json line: %v", err)
+		}
+		if line.ShortUrl == key {
+			isAlreadySaved = true
+		}
+		countLines++
+	}
+
+	if isAlreadySaved {
+		return
+	}
+
+	line := Line{UUID: strconv.Itoa(countLines + 1), ShortUrl: key, OriginalUrl: value}
+	data, err := json.Marshal(&line)
+	if err != nil {
+		log.Fatalf("failed format data to json format: %v", err)
+	}
+	data = append(data, '\n')
+
+	_, err = file.Write(data)
+	if err != nil {
+		log.Fatalf("failed to write in file: %v", err)
+	}
 }
 
 func (s *Storage) Get(key string) (string, bool) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
-	val, ok := s.links[key]
-	return val, ok
+	file, err := os.OpenFile(s.filePath, os.O_RDONLY|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatalf("failed to open storage file: %v", err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := Line{}
+		if err := json.Unmarshal(scanner.Bytes(), &line); err != nil {
+			log.Fatalf("failed to read json line: %v", err)
+		}
+		if line.ShortUrl == key {
+			return line.OriginalUrl, true
+			break
+		}
+	}
+	return "", false
 }
 
-func NewStorage() *Storage {
+func NewStorage(fileStoragePath string) *Storage {
 	return &Storage{
-		links: map[string]string{},
-		mu:    &sync.Mutex{},
+		filePath: fileStoragePath,
+		mu:       &sync.Mutex{},
 	}
 }
