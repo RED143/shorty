45853b30f420870dc764cfbed7194dbcf107834f
RED143 <b.zabalotny@gmail.com>
2023-06-10 18:44:28 +0300

    fix: refactor storages

---
+++ internal/app/storage/fileStorage/fileStorage.go
@@ -0,0 +1,80 @@
+package fileStorage
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strconv"
+	"sync"
+)
+
+type fileStorage struct {
+	mu         *sync.Mutex
+	filePath   string
+	linesCount int
+	links      map[string]string
+}
+
+type fileLine struct {
+	UUID        string `json:"uuid"`
+	ShortURL    string `json:"short_url"`
+	OriginalURL string `json:"original_url"`
+}
+
+func (s *fileStorage) Put(key, value string) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	file, err := os.OpenFile(s.filePath, os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		return fmt.Errorf("failed to open the file \"%s\": %w", s.filePath, err)
+	}
+	defer file.Close()
+
+	s.linesCount += 1
+	line := fileLine{UUID: strconv.Itoa(s.linesCount), ShortURL: key, OriginalURL: value}
+	data, err := json.Marshal(&line)
+	if err != nil {
+		return fmt.Errorf("failed to encode json: %w", err)
+	}
+	data = append(data, '\n')
+
+	_, err = file.Write(data)
+	if err != nil {
+		return fmt.Errorf("failed to save data to file: %w", err)
+	}
+
+	s.links[key] = value
+
+	return nil
+}
+
+func (s *fileStorage) Get(key string) (string, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	val := s.links[key]
+	return val, nil
+}
+
+func CreateFileStorage(filePath string) (*fileStorage, error) {
+	file, err := os.OpenFile(filePath, os.O_RDONLY|os.O_CREATE, 0666)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open the file \"%s\": %w", filePath, err)
+	}
+	defer file.Close()
+
+	storage := &fileStorage{filePath: filePath, linesCount: 0, mu: &sync.Mutex{},
+		links: map[string]string{}}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := fileLine{}
+		if err := json.Unmarshal(scanner.Bytes(), &line); err != nil {
+			return nil, fmt.Errorf("failed to decode json: %w", err)
+		}
+		storage.links[line.ShortURL] = line.OriginalURL
+		storage.linesCount += 1
+	}
+
+	return storage, nil
+}
